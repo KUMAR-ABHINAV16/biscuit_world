e2beed4f5d4825620a2ce2c163481bd3870dd384
unknown <abhinav69596959@gmail.com>
2022-11-04 19:11:16 +0530

    project done

---
+++ app.js
@@ -0,0 +1,10 @@
+   
+
+  function alertify() {
+    if(document.getElementById("name").value == "" || document.getElementById("email").value == "" || document.getElementById("contact").value == "" || document.getElementById("adress").value == "")
+        alert("fill all the details");
+    else
+        alert("Order is Placed");
+
+
+}
