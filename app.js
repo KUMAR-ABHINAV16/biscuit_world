85be013d697032da3878010c9d06b97ea5b84fba
unknown <abhinav69596959@gmail.com>
2022-11-08 22:21:34 +0530

    abhinav

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
    
 
-  function alertify() {
+  function alertify()
+   {
+
     if(document.getElementById("name").value == "" || document.getElementById("email").value == "" || document.getElementById("contact").value == "" || document.getElementById("adress").value == "")
         alert("fill all the details");
     else
         alert("Order is Placed");
 
-
-}
+  }
